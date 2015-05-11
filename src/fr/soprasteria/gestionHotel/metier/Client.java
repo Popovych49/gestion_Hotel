f97caec71ec732126245c093b4152b05a66c0c8b
Popovych49 <lanc.nicolas@gmail.com>
2015-05-11 13:15:43 +0200

    GestionHotel v1.01

--- src/fr/soprasteria/gestionHotel/metier/Client.java
+++ src/fr/soprasteria/gestionHotel/metier/Client.java
@@ -16,56 +16,71 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="client")
-
+@Table(name = "client")
 public class Client {
 
 	@Id
 	@Column(name = "CLIENT_ID", nullable = false)
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String client_id;
-	
-	@Column(name="NOM_CLIENT")
+
+	@Column(name = "NOM_CLIENT")
 	protected String nom_client;
-	
-	@Column(name="PRENOM_CLIENT")
+
+	@Column(name = "PRENOM_CLIENT")
 	protected String prenom_client;
-	
-	@Column(name="ADRESSE_RUE_CLIENT")
+
+	@Column(name = "ADRESSE_RUE_CLIENT")
 	protected String adresse_rue_client;
-	
-	@Column(name="TEL_CLIENT")
+
+	@Column(name = "TEL_CLIENT")
 	protected String tel_client;
-	
-	@Column(name="PARRAIN_ID")
+
+	@Column(name = "PARRAIN_ID")
 	protected String parrain_id;
-	
-	@Column(name="AGE")
+
+	@Column(name = "AGE")
 	protected Integer age;
-	
-	@ManyToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
-	@JoinColumn(name="VILLE_ID",nullable=false)
+
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "VILLE_ID", nullable = false)
 	private Ville ville;
-	
-	@ManyToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
-	@JoinColumn (name="OPERATEUR_ID",nullable=false)
+
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "OPERATEUR_ID", nullable = false)
 	private Operateur operateur;
-	
-	@OneToMany(mappedBy = "clients",cascade=CascadeType.ALL) 
-	private Set<Reservation> reservations = new HashSet<Reservation>(); 
-	
-	@OneToMany(mappedBy = "clients",cascade=CascadeType.ALL) 
+
+	@OneToMany(mappedBy = "clients", cascade = CascadeType.ALL)
+	private Set<Reservation> reservations = new HashSet<Reservation>();
+
+	@OneToMany(mappedBy = "clients", cascade = CascadeType.ALL)
 	private Set<Facture> factures = new HashSet<Facture>();
 
-	Client(){
-	}
-	
-	// toString
-	public String toString() {
-		return String.format("[%s,%s,%s,%s,%s,%s,%d]", getClient_id(), getNom_client(), getPrenom_client(), getAdresse_rue_client(), getTel_client(),getParrain_id(),getAge());
+
+	public Client(String nom_client, String prenom_client,
+			String adresse_rue_client, String tel_client, String parrain_id,
+			Integer age, Ville ville, Operateur operateur) {
+		super();
+		this.nom_client = nom_client;
+		this.prenom_client = prenom_client;
+		this.adresse_rue_client = adresse_rue_client;
+		this.tel_client = tel_client;
+		this.parrain_id = parrain_id;
+		this.age = age;
+		this.ville = ville;
+		this.operateur = operateur;
 	}
 
-	
+	public Client() {
+	}
+
+	// toString
+	public String toString() {
+		return String.format("[%s,%s,%s,%s,%s,%s,%d]", getClient_id(),
+				getNom_client(), getPrenom_client(), getAdresse_rue_client(),
+				getTel_client(), getParrain_id(), getAge());
+	}
+
 	public String getClient_id() {
 		return client_id;
 	}
@@ -152,6 +167,6 @@ public class Client {
 
 	public void setFactures(Set<Facture> factures) {
 		this.factures = factures;
-	} 
+	}
 
 }
